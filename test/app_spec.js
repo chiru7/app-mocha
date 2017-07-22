9fe56e96e3ee0e90370756e5fba7f09d38a1ef85
chiru7 <nagato3c+github@gmail.com>
2017-07-22 18:35:14 +0900

    first commit

---
+++ test/app_spec.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var app = require('../app');
+
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('app', function() {
+
+  describe('/', function() {
+    it ('responds with status 200', function(done) {
+      chai.request(app)
+        .get('/')
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+
+});
